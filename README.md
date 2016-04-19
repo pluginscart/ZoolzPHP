37d2fce5c5bc8a0395c07c627c61c9e8b76f2eca
Bharat <bharatkumardhaker@gmail.com>
2016-04-19 19:06:02 +0530

    All functions updated.

--- README.md
+++ README.md
@@ -31,8 +31,14 @@ Next, update Composer from the Terminal:
 Create an instance of the class while passing in an array including your wsdl service URL and the authToken to work with.
 ```php
 
-	$config_wsdl_url = 'your_wsdl_service_url_HERE'
-	$config_authToken = 'your_auth_token_HERE'
+	$config_wsdl_url = 'your_wsdl_service_url_HERE';
+	
+	//$config_wsdl_url = "http://cloud2.zoolz.com/Services/Reseller/Service.asmx?wsdl";
+	
+	$config_authToken = 'your_auth_token_HERE';
+	
+	//$config_authToken =	'0a2cbe7c787711e3931322000a1e8728';
+	
 	$zoolz = new ZoolzPHP($config_wsdl_url, $config_authToken);
 	
 	// Tell the library to output debug info
@@ -49,9 +55,367 @@ This method takes an int value `$plan_ID` and will attempt to get information of
 
 ```php
 
-	$zoolz->GetPlanInfo('331');
+	$return = $zoolz->GetPlanInfo('331');
 	print "<pre>\n";
-	print_r($users);
+	print_r($return);
 	print "</pre>";
+	
+```
+
+##CreateAccount(array $parameters)
+
+This method takes an array values `$parameters` for new account creation and will attempt to crate account in Zoolz reseller account.
+
+```php
+
+	$parameters_create_account = array (
+				'name'	=> 'bharat', //required 
+				'email'=>'bharatkumardhaker@gmail.com', //required 
+				'password'=>'testbharat',
+				'planID' => '331',		// the ID number for their plan
+				'sendEmail'=>true,
+				//'Language'=>$Language,
+		
+			);
+	$return = $zoolz->CreateAccount($parameters_create_account);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##CreateAccountWithLanguage(array $parameters)
+
+This method takes an array values `$parameters` for new account creation and will attempt to crate account in Zoolz reseller account.
+Here is language code: 
+1 = English
+3 = French 
+5 = Polish
+6 = Finnish
+
+```php
+
+	$parameters_create_account = array (
+				'name'	=> 'bharat', //required 
+				'email'=>'bharatkumardhaker@gmail.com', //required 
+				'password'=>'testbharat',
+				'planID' => '331',		// the ID number for their plan
+				'sendEmail'=>true,
+				'Language'=>$Language, //int
+		
+			);
+	$return = $zoolz->CreateAccountWithLanguage($parameters_create_account);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##CreateAccountWithNetworkDrive(array $parameters)
+
+This method takes an array values `$parameters` for new account creation and will attempt to crate account in Zoolz reseller account.
+
+```php
+
+	$parameters_create_account = array (
+				'name'	=> 'bharat', //required 
+				'email'=>'bharatkumardhaker@gmail.com', //required 
+				'password'=>'testbharat',
+				'planID' => '331',		// the ID number for their plan
+				'sendEmail'=>true,
+				'Language'=>$Language,
+				'enableNetworkDrive'=>true, //Boolean
+		
+			);
+	$return = $zoolz->CreateAccountWithNetworkDrive($parameters_create_account);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##CreateAccountWithComputersLimit(array $parameters)
+
+This method takes an array values `$parameters` for new account creation and will attempt to crate account in Zoolz reseller account.
+
+```php
+
+	$parameters_create_account = array (
+				'name'	=> 'bharat', //required 
+				'email'=>'bharatkumardhaker@gmail.com', //required 
+				'password'=>'testbharat',
+				'planID' => '331',		// the ID number for their plan
+				'sendEmail'=>true,
+				'Language'=>$Language,
+				'enableNetworkDrive'=>true, //Boolean
+				'computersLimit'=>1, //int
+		
+			);
+	$return = $zoolz->CreateAccountWithComputersLimit($parameters_create_account);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##CreatePlan(array $parameters)
+
+This method takes an array values `$parameters` for new plan creation and will attempt to create new plans under the reseller account, giving the ability to choose the allowed instant and cold storage space, the number of users and server allowed, and the subscription frequency.
+
+Plan Type It determines the plan type, when set to 0 the plan type will be Home plan, and when set to 1 the plan type will be Business plan, this value must be passed as integer.
+
+Frequency
+Passed as integer, this will set the frequency time for the plan, as shown below:
+0 = Trial
+1 = Free 
+2 = Monthly
+3 = Yearly
+
+```php
+
+	$parameters = array (
+				'planName'	=> 'bharat', //required 
+				'instantStorageGB'=>'', //int
+				'coldStorageGB'=>'', //int
+				'users' => '',		// int
+				'servers'=>'', //int
+				'type'=>, //int
+				'frequency'=>, //int
+			);
+	$return = $zoolz->CreatePlan($parameters);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+
+##UpgradeAccount(array $parameters)
+
+Use this API to update user’s storage, users by assigning a different plan to a user account.
+
+Plan ID The plan you wish to assign/upgrade the user to. You will need to create plans on the reseller system prior using this option and pass the ID number as an integer.
+
+Note Upgrading the account via will not change the expiry date of the account. To update this information, you will need to call the Change Account Expiry Date API.
+
+
+```php
+
+	$parameters = array (
+				'email'	=> '', //required 
+				'planID'=>'', //int
+			);
+	$return = $zoolz->UpgradeAccount($parameters);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##UpgradeAccountUsingAccountID(array $parameters)
+
+Use this API to update user’s storage, users by assigning a different plan to a user account.
+
+Plan ID The plan you wish to assign/upgrade the user to. You will need to create plans on the reseller system prior using this option and pass the ID number as an integer.
+
+Note Upgrading the account via will not change the expiry date of the account. To update this information, you will need to call the Change Account Expiry Date API.
+
+
+```php
+
+	$parameters = array (
+				'accountID'	=> '', //required 
+				'planID'=>'', //int
+			);
+	$return = $zoolz->UpgradeAccountUsingAccountID($parameters);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##ChangeAccountExpiryDate(array $parameters)
+
+Use this API if you wish to change the expiry date of the account.
+Note: Accounts that have been expired for more than 30 days may be subjected to deletion.
+
+
+```php
+
+	$parameters = array (
+				'email'	=> '', //required 
+				'expiryDate'=>'', //int
+			);
+	$return = $zoolz->ChangeAccountExpiryDate($parameters);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##ChangeAccountExpiryDateUsingAccountID(array $parameters)
+
+Use this API if you wish to change the expiry date of the account.
+Note: Accounts that have been expired for more than 30 days may be subjected to deletion.
+
+
+```php
+
+	$parameters = array (
+				'accountID'	=> '', //required 
+				'expiryDate'=>'', //int
+			);
+	$return = $zoolz->ChangeAccountExpiryDateUsingAccountID($parameters);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+
+##SuspendAccount(string $email)
+
+Use this API to suspend a specific account, if it fails to commit payment for example or upon request.
+
+
+```php
+
+	$return = $zoolz->SuspendAccount($email);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##SuspendAccountUsingAccountID(int $accountID)
+
+Use this API to suspend a specific account, if it fails to commit payment for example or upon request.
+
+
+```php
+
+	$return = $zoolz->SuspendAccountUsingAccountID($accountID);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##ActivateAccount(string $email)
+
+Use this API to activate a suspended account.
+
+```php
+
+	$return = $zoolz->ActivateAccount($email);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##ActivateAccountUsingAccountID(int $accountID)
+
+Use this API to activate a suspended account.
+
+```php
+
+	$return = $zoolz->ActivateAccountUsingAccountID($accountID);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##ToggleNetworkDrive(int $accountID, boolean $isEnabled)
+
+Use this API to activate the ability to use external/network drives for a certain user.
+
+```php
+
+	$return = $zoolz->ToggleNetworkDrive($accountID, $isEnabled);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##DeleteAccount(string $email)
+
+Use this API to delete a specific account.
+Important: Deleting an account will remove all account information and backed up data, so use this API with caution.
+
+```php
+
+	$return = $zoolz->DeleteAccount($email);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##DeleteAccountUsingAccountID(int $accountID)
+
+Use this API to delete a specific account.
+Important: Deleting an account will remove all account information and backed up data, so use this API with caution.
+
+```php
+
+	$return = $zoolz->DeleteAccountUsingAccountID($accountID);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##GetAccountInfo(string $email)
+
+Here you can request account information about a specific user, such as storage space, used space, registration end date and last activity.
+
+```php
+
+	$return = $zoolz->GetAccountInfo($email);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##GetAccountInfoUsingEmailAndPass(string $email, string password )
+
+Here you can request account information about a specific user, such as storage space, used space, registration end date and last activity.
+
+```php
+
+	$return = $zoolz->GetAccountInfoUsingEmailAndPass($email, $password);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##GetAccountInfoUsingAccountID(int $accountID)
+
+Here you can request account information about a specific user, such as storage space, used space, registration end date and last activity.
+
+```php
+
+	$return = $zoolz->GetAccountInfoUsingAccountID($accountID);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
+	
+```
+
+##ResetPassword(string $email, string password )
+
+To change user password using email id. 
+
+```php
+
+	$return = $zoolz->ResetPassword($email, $password);
+	print "<pre>\n";
+	print_r($return);
+	 print "</pre>";	
 	
 ```
--- src/ZoolzPHP.php
+++ src/ZoolzPHP.php
@@ -17,6 +17,7 @@ class ZoolzPHP
    var  $authToken;
    var  $debug;
    var  $error;
+   var $client;
 
    // Constructor
    public function __construct($wsdl_url, $authToken)
@@ -24,6 +25,19 @@ class ZoolzPHP
       $this->wsdl_url = $wsdl_url;
       $this->authToken = $authToken;
       $this->debug;
+	  
+	  $opts = array(
+		'http'=>array(
+		'user_agent' => 'PHPSoapClient'
+		)
+	);
+
+		$context = stream_context_create($opts);
+			
+		$this->client = new SoapClient($this->wsdl_url,
+                        array('stream_context' => $context,
+                              'cache_wsdl' => WSDL_CACHE_NONE));
+								   
    }
 
 
@@ -36,28 +50,21 @@ class ZoolzPHP
 
 	
 
-	public function GetPlanInfo($planID){
+	public function CreateAccount($parameters){
 		try{
-
-			$opts = array(
-				'http'=>array(
-				'user_agent' => 'PHPSoapClient'
-				)
-			);
-
-			$context = stream_context_create($opts);
-			
-			$client = new SoapClient($this->wsdl_url,
-                             array('stream_context' => $context,
-                                   'cache_wsdl' => WSDL_CACHE_NONE));
-
-			$parameters = array (
-				'authToken' => $this->authToken, // api key, to authenticate
-				'planID' => $planID,		// the ID number for their plan
+			$parameters['authToken']=$this->authToken;
+			/*
+			$parameters_create_account = array (
+				'name'	=> 'bharat', //required 
+				'email'=>'bharatkumardhaker@gmail.com', //required
+				'password'=>'testbharat',
+				'planID' => '331',		// the ID number for their plan
+				'sendEmail'=>true,
+				'Language'=>$Language,
 		
 			);
-									   
-			return $client->GetPlanInfo($parameters);
+			*/
+			return $this->client->CreateAccount($parameters);
 			
 		}
 		catch(Exception $e){
@@ -69,8 +76,422 @@ class ZoolzPHP
 
 	}
 
+	public function CreateAccountWithLanguage($parameters){
+		try{
+			$parameters['authToken']=$this->authToken;
+			/*
+			$parameters_create_account = array (
+				'name'	=> 'bharat', //required 
+				'email'=>'bharatkumardhaker@gmail.com', //required
+				'password'=>'testbharat',
+				'planID' => '331',		// the ID number for their plan
+				'sendEmail'=>true,	//boolean
+				'Language'=>$Language,  //int
+		
+			);
+			*/
+			return $this->client->CreateAccountWithLanguage($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
 
 
+	}
+	
+	public function CreateAccountWithNetworkDrive($parameters){
+		try{
+			$parameters['authToken']=$this->authToken;
+			/*
+			$parameters_create_account = array (
+				'name'	=> 'bharat', //required 
+				'email'=>'bharatkumardhaker@gmail.com', //required
+				'password'=>'testbharat',
+				'planID' => '331',		// the ID number for their plan
+				'sendEmail'=>true,
+				'Language'=>$Language, //int
+				'enableNetworkDrive'=>true, //Boolean
+		
+			);
+			*/
+			return $this->client->CreateAccountWithNetworkDrive($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+
+	}
+
+	public function CreateAccountWithComputersLimit($parameters){
+		try{
+			$parameters['authToken']=$this->authToken;
+			/*
+			$parameters_create_account = array (
+				'name'	=> 'bharat', //required 
+				'email'=>'bharatkumardhaker@gmail.com', //required
+				'password'=>'testbharat',
+				'planID' => '331',		// the ID number for their plan
+				'sendEmail'=>true,
+				'Language'=>$Language, //int
+				'enableNetworkDrive'=>true, //Boolean
+				'computersLimit'=>1, //int
+		
+			);
+			*/
+			return $this->client->CreateAccountWithComputersLimit($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+
+	}
+	
+	public function CreatePlan($parameters){
+		try{
+			$parameters['authToken']=$this->authToken;
+			/*
+			$parameters_create_account = array (
+				'planName'	=> 'bharat', //required 
+				'instantStorageGB'=>'', //int
+				'coldStorageGB'=>'', //int
+				'users' => '',		// int
+				'servers'=>'', //int
+				'type'=>, //int
+				'frequency'=>, //int
+				
+		
+			);
+			*/
+			return $this->client->CreatePlan($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+
+	}
+	
+	public function UpgradeAccount($parameters){
+		try{
+			$parameters['authToken']=$this->authToken;
+			/*
+			$parameters_create_account = array (
+					'email'	=> '', //required 
+				'planID'=>'', //int
+			);
+			*/
+			return $this->client->UpgradeAccount($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+
+	}
+	
+
+	public function UpgradeAccountUsingAccountID($parameters){
+		try{
+			$parameters['authToken']=$this->authToken;
+			/*
+			$parameters_create_account = array (
+					'accountID'	=> '', //required 
+				'planID'=>'', //int
+			);
+			*/
+			return $this->client->UpgradeAccountUsingAccountID($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+
+	}
+	
+	public function ChangeAccountExpiryDate($parameters){
+		try{
+			$parameters['authToken']=$this->authToken;
+			/*
+			$parameters_create_account = array (
+					'email'	=> '', //required 
+				'expiryDate'=>'', //int
+			);
+			*/
+			return $this->client->ChangeAccountExpiryDate($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+	}
+	
+	public function ChangeAccountExpiryDateUsingAccountID($parameters){
+		try{
+			$parameters['authToken']=$this->authToken;
+			/*
+			$parameters_create_account = array (
+					'accountID'	=> '', //required 
+				'expiryDate'=>'', //int
+			);
+			*/
+			return $this->client->ChangeAccountExpiryDateUsingAccountID($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+	}
+	
+	public function SuspendAccount($email){
+		try{
+			$parameters['authToken']=$this->authToken;
+			$parameters['email']=$email;
+			
+			return $this->client->SuspendAccount($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+	}
+	
+	public function SuspendAccountUsingAccountID($accountID){
+		try{
+			$parameters['authToken']=$this->authToken;
+			$parameters['accountID']=$accountID;
+			
+			return $this->client->SuspendAccountUsingAccountID($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+	}
+	
+	public function ActivateAccount($email){
+		try{
+			$parameters['authToken']=$this->authToken;
+			$parameters['email']=$email;
+			
+			return $this->client->ActivateAccount($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+	}
+	
+	public function ActivateAccountUsingAccountID($accountID){
+		try{
+			$parameters['authToken']=$this->authToken;
+			$parameters['accountID']=$accountID;
+			
+			return $this->client->ActivateAccountUsingAccountID($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+	}
+	
+	public function ToggleNetworkDrive($accountID, $isEnabled){
+		try{
+			$parameters['authToken']=$this->authToken;
+			$parameters['accountID']=$accountID;
+			$parameters['isEnabled']=$isEnabled;
+			return $this->client->ToggleNetworkDrive($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+	}
+	
+	public function DeleteAccount($email){
+		try{
+			$parameters['authToken']=$this->authToken;
+			$parameters['email']=$email;
+			
+			return $this->client->DeleteAccount($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+	}
+	
+	public function DeleteAccountUsingAccountID($accountID){
+		try{
+			$parameters['authToken']=$this->authToken;
+			$parameters['accountID']=$accountID;
+		
+			return $this->client->DeleteAccountUsingAccountID($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+	}
+	
+	public function GetAccountInfo($email){
+		try{
+			$parameters['authToken']=$this->authToken;
+			$parameters['email']=$email;
+		
+			return $this->client->GetAccountInfo($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+	}
+	
+	public function GetAccountInfoUsingEmailAndPass($email,$password){
+		try{
+			$parameters['authToken']=$this->authToken;
+			$parameters['email']=$email;
+			$parameters['password']=$password;
+			return $this->client->GetAccountInfoUsingEmailAndPass($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+	}
+	
+	public function GetAccountInfoUsingAccountID($accountID){
+		try{
+			$parameters['authToken']=$this->authToken;
+			$parameters['accountID']=$accountID;
+		
+			return $this->client->GetAccountInfoUsingAccountID($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+	}
+	
+	public function GetPlanInfo($planID){
+		try{
+			
+			$parameters = array (
+				'authToken' => $this->authToken, // api key, to authenticate
+				'planID' => $planID,		// the ID number for their plan
+		
+			);
+									   
+			return $this->client->GetPlanInfo($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+
+	}
+	
+	public function GetPlanInfos(){
+		try{
+			
+			$parameters = array (
+				'authToken' => $this->authToken, // api key, to authenticate
+			//	'planID' => $planID,		// the ID number for their plan
+		
+			);
+									   
+			return $this->client->GetPlanInfos($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+
+	}
+	
+	public function ResetPassword($email, $password){
+		try{
+			
+			$parameters = array (
+				'authToken' => $this->authToken, // api key, to authenticate
+				'email' => $email,		// 
+				'password'=>$password, //new password.
+		
+			);
+									   
+			return $this->client->ResetPassword($parameters);
+			
+		}
+		catch(Exception $e){
+			
+			$this->error=$e->getMessage();
+			return false;
+		}
+
+
+	}
+	
    // Debug logging
    function debuglog($message)
    {
